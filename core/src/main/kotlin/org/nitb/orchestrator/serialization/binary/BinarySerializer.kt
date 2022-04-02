cd5cbb56855243f49881024e621038334ce4da8e
nico <nbreval@isotrol.com>
2022-04-02 21:29:21 +0200

    Dynamize binary serializer disabling classes registration

--- core/src/main/kotlin/org/nitb/orchestrator/serialization/binary/BinarySerializer.kt
+++ core/src/main/kotlin/org/nitb/orchestrator/serialization/binary/BinarySerializer.kt
@@ -3,41 +3,30 @@ package org.nitb.orchestrator.serialization.binary
 import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.Input
 import com.esotericsoftware.kryo.io.Output
-import org.nitb.orchestrator.cloud.CloudMessage
 import org.nitb.orchestrator.logging.LoggingManager
-import java.lang.Exception
-import java.math.BigInteger
 
 object BinarySerializer {
 
-    private val conf = ThreadLocal.withInitial {
-        val kryo = Kryo()
-        kryo.register(ByteArray::class.java)
-        kryo.register(Array<ByteArray>::class.java)
-        kryo.register(CloudMessage::class.java)
-        kryo.register(BigInteger::class.java)
-        kryo
-    }
-
+    @Synchronized
     fun encode(message: Any): ByteArray {
-        try {
-            val output = Output(0, Int.MAX_VALUE)
-            conf.get().writeClassAndObject(output, message)
-            return output.toBytes()
-        } catch (e: Exception) {
-            throw e
-        }
+        val output = Output(0, Int.MAX_VALUE)
+        conf.writeClassAndObject(output, message)
+        return output.toBytes()
     }
 
+    @Synchronized
     @Suppress("UNCHECKED_CAST")
     fun <T> decode(bytes: ByteArray): T {
-        try {
-            val input = Input(bytes)
-            return conf.get().readClassAndObject(input) as T
-        } catch (e: Exception) {
-            throw e
-        }
+        val input = Input(bytes)
+        return conf.readClassAndObject(input) as T
     }
 
     private val logger = LoggingManager.getLogger("binary.serializer")
+    private val conf by lazy {
+        val kryo = Kryo()
+        kryo.references = true
+        kryo.isRegistrationRequired = false
+        kryo
+    }
+    private const val classNotRegisteredMessageStart = "Class is not registered:"
 }
