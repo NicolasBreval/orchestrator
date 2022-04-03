66fe9c10f52ffc7407ad6f1a61b4ba1fe1347be4
nico <nbreval@isotrol.com>
2022-04-03 11:06:24 +0200

    Add entity to Subscriptions database table for DSL operations

--- core/src/main/kotlin/org/nitb/orchestrator/database/relational/entities/Subscriptions.kt
+++ core/src/main/kotlin/org/nitb/orchestrator/database/relational/entities/Subscriptions.kt
@@ -1,7 +1,29 @@
 package org.nitb.orchestrator.database.relational.entities
 
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.datetime
+import java.time.LocalDateTime
+
+class SubscriptionEntry(
+    val name: String,
+    val content: ByteArray,
+    val slave: String,
+    val stopped: Boolean,
+    val active: Boolean,
+    val creationDate: LocalDateTime = LocalDateTime.now(),
+    val id: Long? = null
+) {
+    constructor(resultRow: ResultRow): this(
+        resultRow[Subscriptions.name],
+        resultRow[Subscriptions.content].bytes,
+        resultRow[Subscriptions.slave],
+        resultRow[Subscriptions.stopped],
+        resultRow[Subscriptions.active],
+        resultRow[Subscriptions.creationDate],
+        resultRow[Subscriptions.id]
+    )
+}
 
 object Subscriptions: Table("SUBSCRIPTIONS") {
     val id = long("ID").autoIncrement()
