6abc6640731363444db0016eb705c2713ce9d7dd
nico <nbreval@isotrol.com>
2022-04-03 18:25:33 +0200

    Add additional information to subscriber information object used in non-fixed allocation strategies

--- core/src/main/kotlin/org/nitb/orchestrator/subscriber/entities/SubscriberInfo.kt
+++ core/src/main/kotlin/org/nitb/orchestrator/subscriber/entities/SubscriberInfo.kt
@@ -1,9 +1,11 @@
 package org.nitb.orchestrator.subscriber.entities
 
+import com.sun.management.OperatingSystemMXBean
 import org.nitb.orchestrator.annotations.NoArgsConstructor
 import org.nitb.orchestrator.config.ConfigManager
 import org.nitb.orchestrator.config.ConfigNames
 import java.io.Serializable
+import java.lang.management.ManagementFactory
 import java.net.InetAddress
 
 @NoArgsConstructor
@@ -14,5 +16,8 @@ class SubscriberInfo(
     val timestamp: Long = System.currentTimeMillis(),
     val hostname: String = InetAddress.getLocalHost().hostName,
     val ipAddress: String = InetAddress.getLocalHost().hostAddress,
-    val port: Int? = ConfigManager.getInt(ConfigNames.PORT_NUMBER)
+    val port: Int? = ConfigManager.getInt(ConfigNames.PORT_NUMBER),
+    val totalMemory: Long = Runtime.getRuntime().totalMemory(),
+    val freeMemory: Long = Runtime.getRuntime().freeMemory(),
+    val cpuUsage: Double = (ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean).processCpuLoad
 ): Serializable
