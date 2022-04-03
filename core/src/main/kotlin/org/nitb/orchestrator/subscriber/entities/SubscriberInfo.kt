b5ed6a29b03cbe6fc8151b5bdfe6b00a9b3af95f
nico <nbreval@isotrol.com>
2022-04-03 11:05:58 +0200

    Send subscriptions as their JSON representation in SubscriberInfo DTO

--- core/src/main/kotlin/org/nitb/orchestrator/subscriber/entities/SubscriberInfo.kt
+++ core/src/main/kotlin/org/nitb/orchestrator/subscriber/entities/SubscriberInfo.kt
@@ -3,14 +3,13 @@ package org.nitb.orchestrator.subscriber.entities
 import org.nitb.orchestrator.annotations.NoArgsConstructor
 import org.nitb.orchestrator.config.ConfigManager
 import org.nitb.orchestrator.config.ConfigNames
-import org.nitb.orchestrator.subscription.Subscription
 import java.io.Serializable
 import java.net.InetAddress
 
 @NoArgsConstructor
 class SubscriberInfo(
     val name: String,
-    val subscriptions: List<Subscription<*, *>>,
+    val subscriptions: Map<String, String>,
     val isMaster: Boolean,
     val timestamp: Long = System.currentTimeMillis(),
     val hostname: String = InetAddress.getLocalHost().hostName,
